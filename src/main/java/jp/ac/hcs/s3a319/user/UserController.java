83cbb516c9a43cd86c16dd9f211f5a68338944c0
korosuke <korosuke@192.168.10.109>
2021-07-07 18:40:19 +0900

    ユーザー一覧完成

--- src/main/java/jp/ac/hcs/s3a319/user/UserController.java
+++ src/main/java/jp/ac/hcs/s3a319/user/UserController.java
@@ -5,7 +5,11 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,5 +35,40 @@ public class UserController {
 		log.info("[" + principal.getName() + "]ユーザ:" + "表示");
 		
 		return "user/userList";
+	}
+	
+	/**
+	 * ユーザ登録画面(管理者用)を表示する.
+	 * @param form 登録時の入力チェック用UserForm
+	 * @param model
+	 * @return ユーザ登録画面(管理者用)
+	 */
+	@GetMapping("/user/insert")
+	public String getUserInsert(UserForm from,Model model) {
+		return "user/insert";
+	}
+	
+	/**
+	 * 1件分のユーザ情報をデータベースに登録する.
+	 * @param form 登録するユーザ情報(パスワードは平文)
+	 * @param bindingResult データバインド実施結果
+	 * @param principal ログイン情報
+	 * @param model
+	 * @return ユーザー一覧画面
+	 */
+	@PostMapping("/user/insert")
+	public String getUserInsert(@ModelAttribute @Validated UserForm form,
+			BindingResult bindingResult,
+			Principal principal,
+			Model model) {
+		
+		// 入力チェックに引っかかった場合、前の画面に戻る
+		if(bindingResult.hasErrors()) {
+			return getUserInsert(form, model);
+		}
+		
+		//追加処理を実装する
+		
+		return getUserList(principal,model);
 	}
 }
---
+++ src/main/java/jp/ac/hcs/s3a319/user/UserForm.java
@@ -0,0 +1,31 @@
+package jp.ac.hcs.s3a319.user;
+
+import lombok.Data;
+
+/**
+ * フロント-バックでユーザ情報をやり取りする
+ * 各項目のデータ仕様はUserEntityを参照する
+ *
+ */
+@Data
+public class UserForm {
+///**ユーザＩＤ（メールアドレス）*/
+//	@NotBlank(message = "{require_check}")
+//	@Email(message = "{email_check}")
+//	private String user_id;
+//	
+//	/**パスワード*/
+//	@NotBlank(message = "{require_check}")
+//	private String password;
+//	
+//	/**ユーザ名*/
+//	@NotBlank(message = "{require_check}")
+//	private String user_name;
+//	
+//	/**ダークモードフラグ*/
+//	private boolean darkmode;
+//	
+//	/**権限*/
+//	@NotBlank(message = "{require_check}")
+//	private String role;
+}
